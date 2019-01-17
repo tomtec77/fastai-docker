28b96096690d2762fc92e3d1bfae8172c5827e0d
Tomas E. Tecce <tomastecce@gmail.com>
2019-01-17 18:06:34 -0300

    Modified code and data download script.

--- code_data_download.sh
+++ code_data_download.sh
@@ -5,22 +5,32 @@
 
 FASTAI_URL=http://files.fast.ai
 FASTAI_REPO=https://github.com/fastai/fastai.git
+DIR=$HOME/course
+
+if [[ ! -e $DIR ]]; then
+    mkdir $DIR
+fi
 
 # Clone the repository and create the environment
-cd $HOME
+cd $DIR
 git clone $FASTAI_REPO fastai-repo
 cd fastai-repo
 conda env update
 
 # Download and extract the data files
-mkdir $HOME/data
-cd $HOME/data
+cd $DIR
+if [[ ! -e $DIR/data ]]; then
+    mkdir $DIR/data
+fi
+
+cd $DIR/data
 wget --progress=bar:force $FASTAI_URL/data/dogscats.zip
 unzip -q dogscats.zip
-cd $HOME/fastai-repo/courses/dl1/
-ln -s $HOME/data ./
+cd $DIR/fastai-repo/courses/dl1/
+ln -s $DIR/data ./
 
 # Install Jupyterlab
+cd $HOME
 conda activate fastai
 conda install -c conda-forge jupyterlab
 
@@ -30,5 +40,3 @@ echo "c.NotebookApp.ip = '*'" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
 pip install ipywidgets
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
-
-
